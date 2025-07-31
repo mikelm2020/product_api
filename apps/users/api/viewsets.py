b070ff3c71939669367725f22275d68375c7f4a1
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-07-31 10:28:58 -0600

    Added CRUD users

--- apps/users/api/viewsets.py
+++ apps/users/api/viewsets.py
@@ -57,3 +57,47 @@ class UserViewSet(viewsets.GenericViewSet):
 
         serializer = self.list_serializer_class(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request):
+        """
+        Create an user
+        """
+        user_serializer = self.serializer_class(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(
+                {"message": "El usuario se creo correctamente!"},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            {
+                "message": "Hay errores en el registro!",
+                "errors": user_serializer.errors,
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def retrieve(self, request, pk=None):
+        """
+        Get an user
+        """
+        user = self.get_object(pk)
+        user_serializer = self.serializer_class(user)
+        return Response(user_serializer.data)
+
+    @extend_schema(request=UserSerializer)
+    def destroy(self, request, pk=None):
+        """
+        Delete an user in logical mode
+        """
+        user_destroy = self.serializer_class.Meta.model.objects.filter(id=pk).update(
+            is_active=False
+        )
+        if user_destroy == 1:
+            return Response(
+                {"message": "Usuario eliminado correctamente!"},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"message": "El usuario no existe!"}, status=status.HTTP_404_NOT_FOUND
+        )
