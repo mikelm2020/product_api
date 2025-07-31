6449eeb4b2bb3124acd3f2d241abae744b32a777
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-07-31 11:19:35 -0600

    Added model Product

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "descripcion",
+        "precio",
+        "disponible",
+    )
+    search_fields = (
+        "nombre",
+        "precio",
+        "disponible",
+    )
+    ordering = ("precio",)
+    list_filter = (
+        "disponible",
+        "precio",
+    )
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    descripcion = models.TextField(null=True, blank=True)
+    precio = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, blank=False
+    )
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
